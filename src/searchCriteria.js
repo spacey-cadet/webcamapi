fe2ed98eb0808b949cfcded89d6f5d90aa293e3d
TheoKorir <theo.korirchbng@gmail.com>
2024-02-08 22:05:00 +0300

    New Config babel transpiler for reac t webpack for compiling js modules eslint for linting code prettier for best practices for neat code

--- src/searchCriteria.js
+++ src/searchCriteria.js
@@ -1,8 +1,7 @@
 import { useGetCategoriesQuery,useGetCountriesQuery,
     useGetContinentsQuery } from "./features/apiSlice"
-    import { Outlet } from "react-router"
-    import { Link } from "react-router-dom"
-
+import { Outlet } from "react-router"
+import { Link } from "react-router-dom"
 
 const SearchCriteriaComponent = () => {
   
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports={
+    devtool: "source-map",
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'js/[name].[contenthash].js',
+        assetModuleFilename: '[name].[ext]',
+        clean: true
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()]
+    },
+    module:{
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/i,
+                //using MiniCssExtractPlugin sicen our app is already in production
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.(png|jpeg|svg|gif|ico|jpg)$/i,
+                type: 'asset/resource'
+            }
+        ],
+    },
+    plugin: [
+            new HtmlWebpackPlugin({
+                title: "HarambeeStats",
+                filename: 'index.html',
+                template: './public/index.html',
+                favicon: './public/HarambeeStatsFavicon.svg'
+            }),
+            //instead of eslint loader
+            new ESLintPlugin(),
+            //plugin to separate css files so they can be cached in production
+            new MiniCssExtractPlugin({
+                filename: 'css/[name][contenthash].css'
+            })
+        ]
+}
